45bed6bedd597c7d7da0f45db828de5fd78ccd29
Mariusz Bełtowski <mariusz.beltowski@itelo.pl>
2017-12-08 15:50:09 +0100

    Working todo with LocalStorage

---
+++ src/app/input/input.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'todo-input',
+  template: `
+    <input class="todo-input" [value]="title"
+      (keyup.enter)="changeTitle(inputElement)"
+      #inputElement>
+    <button (click)="changeTitle(inputElement)">Save</button>
+    <!-- <p>The title is: {{ title }}</p> -->
+  `,
+  styleUrls: ['./input.component.css']
+})
+export class InputComponent implements OnInit {
+
+  @Output() submit: EventEmitter<string> = new EventEmitter();
+  
+  title: string = 'My First Todo Title !!!';  
+
+  constructor() { }
+
+  ngOnInit() {
+    /* setTimeout(() => {
+      this.title = 'This is not the title you are looking for';  
+    }, 3000); */
+  }
+
+  changeTitle(inputElementRference): void {
+    console.log(inputElementRference);
+    this.title = inputElementRference.value;
+    this.submit.emit(this.title)
+  }
+
+}
---
+++ src/app/list-manager/list-manager.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { TodoListService } from '../todo-list.service';
+
+@Component({
+  selector: 'todo-list-manager',
+  template: `
+      <!--The content below is only a placeholder and can be replaced.-->
+    <div class="todo-app">
+      <h1>
+        Welcome to {{ title }}!
+      </h1>
+      <todo-input (submit)="addItem($event)"></todo-input>
+      <ul>
+        <li *ngFor="let item of todoList">
+          <todo-item [todoItem]="item" (remove)="removeItem($event)"></todo-item>
+        </li>
+      </ul>
+    </div>
+  `,
+  styleUrls: ['./list-manager.component.css']
+})
+export class ListManagerComponent implements OnInit {
+  title = 'todo';
+  todoList: any[];
+  
+  ngOnInit() {
+    this.todoList = this.todoListService.getTodoList();
+  }
+
+  constructor(private todoListService: TodoListService) { }
+
+  addItem(title): void {    
+    this.todoList = this.todoListService.addItem({ title });
+  }
+
+  removeItem(item) {
+    this.todoList = this.todoListService.removeItem(item);
+  }
+
+}
